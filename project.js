607ebcf7b0a9151a360952c79d5e21b561a285e7
avram <avramy.kunin@gmail.com>
2025-08-14 14:46:13 +0300

    pushing today

--- project.js
+++ project.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("main-tabs-content").style.visibility = "visible";
+closeAll();
+document.getElementById("first-tab-content").style.display = "flex";
 })
+
+function closeAll(){
+    const tabs = document.querySelectorAll(".tab");
+    tabs.forEach(tab => {
+        tab.style.display = "none";
+})
+}
 
 function onClick(id){
     switch(id){
         case "first-tab":
-            document.getElementById(id).style.visibility = "visible";
+            closeAll();
+            document.getElementById("first-tab-content").style.display = "flex";
+            break;
+        case "second-tab":
+            closeAll();
+            document.getElementById("second-tab-content").style.display = "flex";
+            break;
     }
 }
